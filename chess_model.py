fix data leakage, include lstm layer to transformer, adj hyparam

chess_model.py
@@ -13,33 +13,28 @@ class PositionalEncoding(nn.Module):
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
-
     def forward(self, x):
         x = x + self.pe[:, :x.size(1)]
         return self.dropout(x)
-        
+
 class ChessModel(nn.Module):
     def __init__(self, vocab_size, embed_dim=64, hidden_dim=128, n_layers=2, dropout=0.1, nhead=4):
         super(ChessModel, self).__init__()
-        self.use_transformer = use_transformer
         self.embed = nn.Embedding(vocab_size, embed_dim)
         self.dropout = nn.Dropout(dropout)
         self.positional_encoding = PositionalEncoding(embed_dim, dropout)
-        encoder_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=nhead, dropout=dropout, batch_first=True)
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=n_layers)
-        self.fc_rating = nn.Linear(embed_dim, 1)
-
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=nhead, dropout=dropout, batch_first=False)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=n_layers)
+        self.lstm = nn.LSTM(embed_dim, hidden_dim, num_layers=n_layers, batch_first=False, dropout=dropout)
+        self.fc_rating = nn.Linear(hidden_dim, 1)
     def forward(self, x):
         x = self.embed(x)
         x = self.dropout(x)
-        if self.use_transformer:
-            x = self.positional_encoding(x)
-            x = x.transpose(0, 1)
-            x = self.transformer_encoder(x)
-            x = x.transpose(0, 1)
-            ratings = self.fc_rating(x)
-        else:
-            output, (h_n, c_n) = self.lstm(x)
-            output = self.dropout(output)
-            ratings = self.fc_rating(output)
+        x = self.positional_encoding(x)
+        x = x.transpose(0, 1)
+        x = self.transformer_encoder(x)
+        x, _ = self.lstm(x)
+        x = self.dropout(x)
+        x = x.transpose(0, 1)
+        ratings = self.fc_rating(x)
         return ratings

train.py
@@ -28,8 +28,7 @@ model = ChessModel(vocab_size=wandb.config.vocab_size,
 criterion_mse = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=wandb.config.learning_rate, weight_decay=wandb.config.weight_decay)
 
-# data = data_full.sample(n=wandb.config.dataset_size, random_state=wandb.config.seed).reset_index(drop=True)
-df, df_val = train_test_split(data_full, test_size=0.01, random_state=wandb.config.seed)  # TODO USE SET 5 FOR TESTING!
+df_train, df_val = train_test_split(data_full, test_size=0.01, random_state=wandb.config.seed)  # TODO USE SET 5 FOR TESTING!
 
 sequences_val = [torch.tensor(ast.literal_eval(seq), dtype=torch.long) for seq in df_val["moves_encoded"]]
 X_val = pad_sequence(sequences_val, batch_first=True, padding_value=0)
@@ -38,20 +37,20 @@ ratings_val = torch.tensor(df_val["white_rating_scaled"].values, dtype=torch.flo
 val_dataset = TensorDataset(X_val, ratings_val)
 val_data_loader = DataLoader(val_dataset, batch_size=wandb.config.batch_size, shuffle=True)
 
-def reload_data(data_full, seed):
+def reload_data(data, seed):
     print("Reloading data with seed ", seed)
-    data = data_full.sample(n=wandb.config.dataset_size, random_state=seed).reset_index(drop=True)
+    sample = data.sample(n=wandb.config.dataset_size, random_state=seed).reset_index(drop=True)
 
-    sequences = [torch.tensor(ast.literal_eval(seq), dtype=torch.long) for seq in data["moves_encoded"]]
+    sequences = [torch.tensor(ast.literal_eval(seq), dtype=torch.long) for seq in sample["moves_encoded"]]
     X = pad_sequence(sequences, batch_first=True, padding_value=0)
-    ratings = torch.tensor(data["white_rating_scaled"].values, dtype=torch.float).unsqueeze(1)
+    ratings = torch.tensor(sample["white_rating_scaled"].values, dtype=torch.float).unsqueeze(1)
 
     dataset = TensorDataset(X, ratings)
     data_loader = DataLoader(dataset, batch_size=wandb.config.batch_size, shuffle=True)
 
     return data_loader
 
-data_loader = reload_data(data_full, wandb.config.seed)
+data_loader = reload_data(df_train, wandb.config.seed)
 
 best_val_loss = float('inf')
 reload_interval = wandb.config.reload_interval
@@ -59,7 +58,7 @@ reload_interval = wandb.config.reload_interval
 # train loop
 for epoch in range(wandb.config.num_epochs):
     if epoch % reload_interval == 0 and epoch != 0:
-        data_loader = reload_data(data_full, wandb.config.seed + epoch)
+        data_loader = reload_data(df_train, wandb.config.seed + epoch)
 
     loss = 0.0
     for X_batch, ratings_batch in data_loader: